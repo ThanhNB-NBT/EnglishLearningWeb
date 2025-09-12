75c614391d72fb1a225434e3069bd1f9ca113828
ThanhNB <nguyenthanh120223@gmail.com>
2025-09-12 14:14:40 +0700

    Initial commit

---
+++ src/main/java/com/thanhnb/englishlearning/EnglishlearningApplication.java
@@ -0,0 +1,13 @@
+package com.thanhnb.englishlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishlearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishlearningApplication.class, args);
+	}
+
+}
